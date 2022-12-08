9ababd8db54a90932eb862c9ec6378c0068dd179
repturr <repturryt@gmail.com>
2022-12-08 06:23:42 -0500

    Update standfarm.lua

--- StandUpright/standfarm.lua
+++ StandUpright/standfarm.lua
@@ -178,7 +178,7 @@ local function AutoRoll()
             local Roka = GetTool("Rokakaka");
             local Arrow = GetTool(ArrowType);
     
-            if Roka and Arrow then
+            if Roka or Arrow then
                 UsingVariable = true;
                 UnequipAll();
                 Use(Roka);
