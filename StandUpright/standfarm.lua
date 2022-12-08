117d02c450f6af54c86d5bb32f4555fae731c8e2
repturr <repturryt@gmail.com>
2022-12-08 07:53:24 -0500

    Update standfarm.lua

--- StandUpright/standfarm.lua
+++ StandUpright/standfarm.lua
@@ -16,7 +16,6 @@ local Webhook = Settings.Webhook;
 local request = syn.request;
 
 --++ Get Services --++--
-local CollectionService = game:GetService("CollectionService")
 local ReplicatedStorage = game:GetService("ReplicatedStorage");
 local StarterGui = game:GetService("StarterGui");
 local Players = game:GetService("Players");
@@ -38,8 +37,6 @@ local AttributeData = Data.Attri
 local FunctionConnections = {
     AutoBuyActive = false;
     AutoRollActive = false;
-    AutoRollUsingItem = false;
-    AutoRollInUse = false;
 };
 local StartupConfirmed = false;
 
@@ -88,11 +85,35 @@ local function Buy(Item)
 	BuyItemEvent:FireServer("MerchantAU", ItemToBuy);
 end;
 
-local function AutoBuy()
+local function Use(Item)
+   UnequipAll();
+
+	EquipTool(Item);
+	Item:FindFirstChild("Use"):FireServer();
+end;
+
+local function CreateMessage(Message)
+	StarterGui:SetCore("ChatMakeSystemMessage",{
+		['Text'] = Message,
+		['Color'] = Color3.fromRGB(44, 180, 52),
+		['Font'] = Enum.Font.SourceSansLight
+	});
+end;
+
+local function CreateNotification(Message, Duration)
+    StarterGui:SetCore("SendNotification", {
+        Title = "SUR Stand Farm";
+        Text = Message;
+        Duration = Duration;
+    });
+end;
+
+local function Autobuy()
     FunctionConnections.AutoBuyActive = not FunctionConnections.AutoBuyActive;
 
-    while FunctionConnections.AutoBuyActive == true do
-        task.wait(BuyItemInterval);
+    task.spawn(function()
+        while FunctionConnections.AutoBuyActive == true do
+            task.wait(BuyItemInterval);
             local OwnsArrows = false;
             local OwnsRokas = false;
 
@@ -128,35 +149,48 @@ local function AutoBuy()
                 if ArrowType ~= "Charged Arrow" then
                 Buy("Stand Arrow");
                 end;
+            end
+        end;
+    end);
+end;
+
+local function AutoRoll()
+    local UsingVariable = false;
+    FunctionConnections.AutoRollActive = not FunctionConnections.AutoRollActive;
+
+        task.wait(1);
+        do
+            if (StandData.Value) == "None" then
+                local Arrow = GetTool(ArrowType);
+
+                if (Arrow) then
+                    Use(Arrow);
             end;
-    end
-end
+        end;
+        UsingVariable = false;
 
-local function Use(Item)
-    UnequipAll();
-	EquipTool(Item);
+        while FunctionConnections.AutoRollActive == true do
+            task.wait(.25);
+            if UsingVariable == true then
+                return;
+            end;
 
-	Item:FindFirstChild("Use"):FireServer();
+            local Roka = GetTool("Rokakaka");
+            local Arrow = GetTool(ArrowType);
+    
+            if Roka or Arrow then
+                UsingVariable = true;
+                UnequipAll();
+                Use(Roka);
+                task.wait(2);
+                UnequipAll();
+                Use(Arrow);
+                task.wait(1);
+                UsingVariable = false;
+            end;
+        end
+    end);
 end;
-
-
-local function CreateMessage(Message)
-	StarterGui:SetCore("ChatMakeSystemMessage",{
-		['Text'] = Message,
-		['Color'] = Color3.fromRGB(44, 180, 52),
-		['Font'] = Enum.Font.SourceSansLight
-	});
-end;
-
-local function CreateNotification(Message, Duration)
-    StarterGui:SetCore("SendNotification", {
-        Title = "SUR Stand Farm";
-        Text = Message;
-        Duration = Duration;
-    });
-end;
-
-
 
 
 local function WebhookMessage(Message, Color)
@@ -215,22 +249,21 @@ end);
 local function RunScript(Value)
     local Functions = {
         [true] = function()
-          if (StandData.Value ~= "None") then
-            CreateNotification("Warning this will ROKA your current stand in 10 seconds \n " .. StandData.Value .. " / " .. AttributeData.Value);
-            task.wait(10);
-
+          local WarningNotification = CreateNotification("Warning this will ROKA your current stand in 10 seconds \n " .. StandData.Value .. " / " .. AttributeData.Value);
+          task.wait(10);
+          
+          do
             local Roka = GetTool("Rokakaka");
 
             if Roka then
                 Use(Roka);
             elseif not Roka then
                 Buy("Rokakaka");
-                task.wait(.15);
+                task.wait(.25);
                 Roka = GetTool("Rokakaka");
-
                 Use(Roka);
-            end
-          end
+            end;
+          end;
 
           local Platform = Instance.new("Part", workspace); 
           Platform.Size = Vector3.new(50, 50, 50);
@@ -243,64 +276,47 @@ local function RunScript(Value)
           Character.HumanoidRootPart.CFrame = Platform.CFrame * CFrame.new(0, 5, 0);
           CreateMessage("SUR Stand Farm: Starting Farm");
 
-          local Arrow = GetTool(ArrowType);
+          Player.Idled:Connect(function()
+            local VirtualUser = game:GetService("VirtualUser")
+            VirtualUser:CaptureController()
+            VirtualUser:ClickButton2(Vector2.new())
+        end)
 
-          if Arrow then
-            Use(Arrow);
-          elseif not Arrow then
-            Buy("Stand Arrow");
-            task.wait(.15);
-            Arrow = GetTool("Stand Arrow");
-            Use(Arrow);
-          end;
+          Autobuy();
+          AutoRoll();
 
-          AutoBuy();
+          task.spawn(function()
+            while task.wait() do
+                if (Settings.Attributes[AttributeData.Value] and Settings.Attributes[AttributeData.Value] == true) then
+                    Autobuy();
+                    AutoRoll();
+                    CreateMessage("Got Stand: " .. StandData.Value);
+                    CreateMessage("Got Attribute: " .. AttributeData.Value);
+                    WebhookMessage("Stats: "  .. StandData.Value  .. "/" ..  AttributeData.Value,  tonumber(0x40ff00));
 
-          local UsingTool = false;
-
-          StandData:GetPropertyChangedSignal("Value"):Connect(function()
-                if Settings.Attributes[AttributeData.Value] and Settings.Attributes[AttributeData.Value] == true then
-                    if Settings.KickUponRoll then
-                        Player:Kick();
+                    if Settings.KickUponRoll == true then
+                        Player:Kick("Requested Kick Upon Roll \n " .. StandData.Value .. "/" .. AttributeData.Value);
                     end
-                    CreateMessage("Stand: " .. StandData.Value .. "\n" .. "Attribute: " .. AttributeData.Value);
-                    WebhookMessage("Stand: " .. StandData.Value .. "\n" .. "Attribute: " .. AttributeData.Value, tonumber(0xffd500));
-                    AutoBuy();
-                    return;
-                else
-                    while Settings.Attributes[AttributeData.Value] and Settings.Attributes[AttributeData.Value] == false  do
-                        task.wait();
 
-                        if UsingTool == true then
-                            return;
-                        end
-
-                        local Roka = GetTool("Rokakaka");
-                        local Arrow = GetTool(ArrowType);
-
-                        if Roka and Arrow then
-                            UsingTool = true;
-                            Use(Roka);
-                            task.wait(1);
-                            Use(Arrow);
-                            task.wait(2);
-                            UsingTool = false;
-                        end
-                    end
-                end
-
-                if Settings.Stands[StandData.Value] and Settings.StandData[StandData.Value] == true then
-                    if Settings.KickUponRoll then
-                        Player:Kick();
-                    end
-                    CreateMessage("Stand: " .. StandData.Value .. "\n" .. "Attribute: " .. AttributeData.Value);
-                    WebhookMessage("Stand: " .. StandData.Value .. "\n" .. "Attribute: " .. AttributeData.Value, tonumber(0xffd500));
-                    AutoBuy();
-                    return;
-                end
-
+                    task.wait(.50);
+                    Platform:Destroy();
+                    break;
+                end;
+    
+                if (Settings.Stands[StandData.Value] and Settings.Stands[StandData.Value] == true) then
+                    Autobuy();
+                    AutoRoll();
+                    CreateMessage("Got Stand: " .. StandData.Value);
+                    CreateMessage("Got Attribute: " .. AttributeData.Value);
+                    WebhookMessage("Stats: "  .. StandData.Value  .. "/" ..  AttributeData.Value,  tonumber(0x40ff00));
+                  
+                    task.wait(.50);
+                    Platform:Destroy();
+                    break;
+                end;
+            end
           end)
-         
+
         end,
         [false] = function()
             
