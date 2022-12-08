c7b02639d82743a8bd5f15dbc6428b98dc307d12
repturr <86138709+repturr@users.noreply.github.com>
2022-12-08 12:42:47 -0500

    Update standfarm.lua

--- StandUpright/standfarm.lua
+++ StandUpright/standfarm.lua
@@ -1,3 +1,5 @@
+task.wait(10);
+
 --++-- Game Services --++--
 local HttpService = game:GetService("HttpService")
 local Players = game:GetService("Players")
@@ -50,24 +52,14 @@ local Messages = {
 
 
 --++-- Player Variables --++--
-task.wait(5);
-
 local Player = Players.LocalPlayer;
 local PlayerHWID = RbxAnalyticsService:GetClientId();
 
-print("Check 1")
-
 local Backpack = Player.Backpack;
-
-print("Check 2")
 
 local Character = Player.Character or Player.CharacterAdded:Wait();
 
-print("Check 3")
-
 local Data = Player.Data;
-
-print("Check 4")
 
 local PlayerStand = Data.Stand;
 local PlayerAttribute = Data.Attri;
